c2953fa2dbe6181a8bbf9fa4e0ab7c2cc5002e69
nmtoan91 <nmtoan91@gmail.com>
2025-04-02 10:28:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TensorBeliefFunction", 
-    version="0.0.2",
+    version="0.0.3",
     author="nmtoan91",
     author_email="toan_stt@yahoo.com",
     description="Tensor of Belief Function",
